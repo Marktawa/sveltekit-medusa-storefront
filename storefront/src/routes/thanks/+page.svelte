8a294e5b08a2e51b3dbc1dfcbfc8ab3fdbe728a7
Mark Munyaka <markmunyaka@gmail.com>
2024-06-03 17:09:15 +0000

    Added UI: Success Page

--- storefront/src/routes/thanks/+page.svelte
+++ storefront/src/routes/thanks/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Success!</h1>
-<p>Payment was successfully processed.</p>
+<h2>Success!</h2>
+<section id="success">
+    <p>Payment was successfully processed.</p>
+</section>
+
+
+<style>
+    h2 {
+        text-align: center;
+        font-size: 3rem;
+        padding: 4rem 0 2rem 0;
+    }
+
+    #success {
+        margin-left: auto;
+        margin-right: auto;
+        margin-bottom: 8rem;
+        max-width: 24rem;
+        padding: 0 0.5rem;
+    }
+
+    p {
+        text-align: center;
+    }
+</style>
